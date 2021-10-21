9bf705bd8306a0edd10f273603c4601b513a2608
Niharikas <sniharika.221200@gmail.com>
2021-10-21 17:43:29 +0530

    Day 2

---
+++ print1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+/*.....printing begins....*/
+printf("I see, I remember\n");
+/*....printing ends......*/
+printf("I see, \n");
+printf("I remember !");
+printf("\nI see,\nI remember !");
+printf("\nI\n.. see,\n... ... ...I\n... ... ... remember !");
+}
