4e1e4e7159af4c7d9e088a49dbcf9177241ed4da
Niharikas <sniharika.221200@gmail.com>
2021-11-06 12:25:42 +0530

    3

--- wordig.c
+++ wordig.c
@@ -8,35 +8,25 @@ q=n/10;
 rem=n%10;
 printf("\nrem :%d",rem);
 switch(rem){
-case 1:if(rem==1)
-printf("\nThe digits are:one (%d)",rem);
+case 1:printf("\nThe digits are:one (%d)",rem);
 break;
-case 2:if(rem==2)
-printf("\nThe digits are:two (%d)",rem);
+case 2:printf("\nThe digits are:two (%d)",rem);
 break;
-case 3:if(rem==3)
-printf("\nThe digits are:three (%d)",rem);
+case 3:printf("\nThe digits are:three (%d)",rem);
 break;
-case 4:if(rem==4)
-printf("\nThe digits are:four (%d)",rem);
+case 4:printf("\nThe digits are:four (%d)",rem);
 break;
-case 5:if(rem==5)
-printf("\nThe digits are:five (%d)",rem);
+case 5:printf("\nThe digits are:five (%d)",rem);
 break;
-case 6:if(rem==6)
-printf("\nThe digits are:six (%d)",rem);
+case 6:printf("\nThe digits are:six (%d)",rem);
 break;
-case 7:if(rem==7)
-printf("\nThe digits are:seven (%d)",rem);
+case 7:printf("\nThe digits are:seven (%d)",rem);
 break;
-case 8:if(rem==8)
-printf("\nThe digits are:eight (%d)",rem);
+case 8:printf("\nThe digits are:eight (%d)",rem);
 break;
-case 9:if(rem==9)
-printf("\nThe digits are:nine (%d)",rem);
+case 9:printf("\nThe digits are:nine (%d)",rem);
 break;
-case 0:if(rem==0)
-printf("\nThe digits are:zero (%d)",rem);
+case 0:printf("\nThe digits are:zero (%d)",rem);
 break;
 default:printf("\nThere is some error...");
 break;}
