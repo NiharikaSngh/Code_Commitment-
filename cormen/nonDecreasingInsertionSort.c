d7e770a232fa0c6ead196c9bff713338d3b7b466
Niharikas <sniharika.221200@gmail.com>
2021-12-01 17:07:34 +0530

    New Folder

---
+++ cormen/nonDecreasingInsertionSort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+	int i, j, key, size;
+
+	printf("\nEnter the size of Array : ");
+ 	scanf("%d", &size);
+
+	int Array[size];
+	printf("Enter Array elements : ");
+
+	for (int k=1; k<=size; k++)
+	{
+		scanf("%d", &Array[k]);
+	}
+
+	for (j=2; j<=size; j++)
+	{
+		key = Array[j];
+		i = j-1;
+		while (i > 0 && Array[i] > key){
+		Array[i+1] = Array[i];
+		i = i-1;
+		Array[i+1] = key;}
+	}
+
+	printf("The Sorted Array : ");
+
+	for (int k=1; k<=size; k++)
+	{
+		printf("%d ", Array[k]);
+	}
+
+	printf("\n\n");
+	}
+	
